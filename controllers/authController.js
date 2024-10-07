bc356dcec887111873390d86bdb9198bcc86650d
LobitoRG <adramirezga@ittepic.edu.mx>
2024-10-06 21:37:46 -0700

    hola

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const JWT_SECRET = 'claveSecreta';
 const JWT_EXPIRES_IN = '5m';
+const { auth, db } = require('../config/firebaseConfig');
+
 
 async function login(req, res) {
     const { username, password } = req.body;
